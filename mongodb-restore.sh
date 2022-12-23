d05a4376beadbc6f5debfe4e298d55ddfd6b5765
[S] <sandeepk.l337@gmail.com>
2022-12-23 15:13:25 +0530

    Create mongodb-restore.sh

---
+++ mongodb-restore.sh
@@ -0,0 +1,64 @@
+# Set the source and destination directories
+src="user@remote.server:/path/to/backup/directory"
+dst="/path/to/mongodb/data/directory"
+
+# Set the email address to send alerts to
+email_address="your@email.com"
+
+# Set the log file
+log_file="/path/to/log/file.log"
+
+# Decrypt the password using the private key
+password=$(gpg --decrypt password.gpg)
+
+# Check if password decryption was successful
+if [ $? -ne 0 ]; then
+  echo "$(date): Decryption of password failed." >> "$log_file"
+  echo "The decryption of the password for the MongoDB backup failed." | mail -s "Restore Failure" "$email_address"
+  exit 1
+fi
+
+# Create a temporary directory to store the decrypted backups
+tmp_dir=$(mktemp -d)
+
+# Download the encrypted backup from the remote server
+rsync -av "$src" "$tmp_dir"
+if [ $? -ne 0 ]; then
+  echo "$(date): Download of the MongoDB backup from $src failed." >> "$log_file"
+  echo "The download of the MongoDB backup from $src failed." | mail -s "Restore Failure" "$email_address"
+  rm -rf "$tmp_dir"
+  exit 1
+fi
+
+# Decrypt the backup using openssl
+openssl enc -d -aes-256-cbc -in "$tmp_dir/encrypted.tar.gz" -out "$tmp_dir/decrypted.tar.gz" -pass "pass:$password"
+if [ $? -ne 0 ]; then
+  echo "$(date): Decryption of the MongoDB backup failed." >> "$log_file"
+  echo "The decryption of the MongoDB backup failed." | mail -s "Restore Failure" "$email_address"
+  rm -rf "$tmp_dir"
+  exit 1
+fi
+
+# Extract the tar archive to the temporary directory
+tar -xzvf "$tmp_dir/decrypted.tar.gz" -C "$tmp_dir"
+if [ $? -ne 0 ]; then
+  echo "$(date): Extraction of the MongoDB backup failed." >> "$log_file"
+  echo "The extraction of the MongoDB backup failed." | mail -s "Restore Failure" "$email_address"
+  rm -rf "$tmp_dir"
+  exit 1
+fi
+
+# Restore the MongoDB database from the dump
+mongorestore --host localhost --db mydatabase "$tmp_dir/mydatabase"
+if [ $? -ne 0 ]; then
+  echo "$(date): Restore of the MongoDB database failed." >> "$log_file"
+  echo "The restore of the MongoDB database from the backup at $src failed." | mail -s "Restore Failure" "$email_address"
+  rm -rf "$tmp_dir"
+  exit 1
+else
+  echo "The restore of the MongoDB database from the backup at $src completed successfully." >> "$log_file"
+  echo "The restore of the MongoDB database from the backup at $src completed successfully." | mail -s "Restore Success" "$email_address"
+fi
+
+# Clean up the temporary directory
+rm -rf "$tmp_dir"
